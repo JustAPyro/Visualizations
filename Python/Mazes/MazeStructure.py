643128d4d3d1feb6d36bc3bbe7e3ab7343508c9f
Luke Hanna <Luke.m.hanna@gmail.com>
2021-05-27 00:42:51 -0400

    MazeFrame now generatese a window with desired size and dimensions

--- Python/Mazes/MazeStructure.py
+++ Python/Mazes/MazeStructure.py
@@ -4,12 +4,28 @@ from tkinter import *
 # Creating our own window class that inherits Frame
 class MazeFrame(Frame):
     title = "Maze"
-    def __init__(self, newTitle):
+    def __init__(self, newTitle, window, dimensions, size):
         # Start by calling the parent Frame initialization
         super().__init__()
 
+        # Set a default border
+        self.border = 25
+
         # Set the title to the provided title
         self.title = newTitle
+
+        # Calculate size of window
+        win_dimensions = str(dimensions[0] * size + self.border * 2)
+        win_dimensions += "x" + str(dimensions[1] * size + self.border * 2)
+
+        # Set the size of the window
+        window.geometry(win_dimensions)
+
+        # Save the dimensions of the cells
+        self.dimensions = dimensions
+
+        # Save the size of each square
+        self.size = size
 
         # Draw everything on the frame
         self.draw()
@@ -25,9 +41,25 @@ class MazeFrame(Frame):
         # Create a canvas to draw our maze on
         canvas = Canvas(self)
 
+
         # here is where we draw stuff
-        for i in range(0, 30, 1):
-            pass
+
+        # For each horizontal line (add 1 because we need 3 lines to draw 2 boxes)
+        for x in range(self.dimensions[0] + 1):
+            canvas.create_line(x * self.size + self.border, self.border, x * self.size + self.border, self.border + self.size * self.dimensions[1])
+
+            # For each vertical line
+            for y in range(self.dimensions[1] + 1):
+                canvas.create_line(self.border, y * self.size + self.border, self.border + self.size * self.dimensions[0], y * self.size + self.border)
+
+
 
         canvas.pack(fill=BOTH, expand=1)
+
+    def move(self):
+        self.x += 1;
+        self.y += 1;
+
+
+
 
--- Python/Mazes/Prim_Generator.py
+++ Python/Mazes/Prim_Generator.py
@@ -4,6 +4,12 @@ from MazeStructure import MazeFrame;
 # Creating a Tkinter window to hold the GUI
 window = Tk()
 
-# Creating an empty frame
-maze = MazeFrame("Prim's Maze Generation")
+# Static variables to start with
+# TODO: Add a small popup to get these values  from user
+dimensions = (15, 25)
+size = 30
+
+# Creating an empty frame, passing in the window, dimensions of maze, and size of cell
+maze = MazeFrame("Prim's Maze Generation", window, dimensions, size)
+
 window.mainloop()
