f6ddfafc2dc4107a5cf54747f2a7e6aa28e8b4a0
Mythical Moose <OfficialMythicalMoose@gmail.com>
2021-05-31 21:35:44 -0400

    Added coloration and prims step 1

--- Python/Mazes/MazeStructure.py
+++ Python/Mazes/MazeStructure.py
@@ -4,9 +4,29 @@ from tkinter import *
 # Creating our own window class that inherits Frame
 class MazeFrame(Frame):
     title = "Maze"
+
+    h_walls = []
+    v_walls = []
+    cells = []
+
     def __init__(self, newTitle, window, dimensions, size):
         # Start by calling the parent Frame initialization
         super().__init__()
+
+        self.color_normal_fill = "#A9D"
+        self.color_select_fill = "#3C4"
+        self.color_maze_fill = "#F93"
+
+        self.color_normal_stroke = "#E93"
+        self.color_select_stroke = "#13F"
+
+        # for dimensions = (2, 2)
+        # Configure the correct number of horizontal and vertical walls
+        self.vertical = [[1 for x in range(dimensions[1])] for y in range(dimensions[0]+1)]  # Horizontal
+        self.horizontal = [[1 for x in range(dimensions[1]+1)] for y in range(dimensions[0])]  # vertical
+
+        # Create a cell array
+        self.cells = [[1 for x in range(dimensions[1])] for y in range(dimensions[0])]
 
         # Set a default border
         self.border = 25
@@ -39,26 +59,51 @@ class MazeFrame(Frame):
         self.pack(fill=BOTH, expand=1)
 
         # Create a canvas to draw our maze on
-        canvas = Canvas(self)
+        self.canvas = Canvas(self)
+        self.draw_update()
 
+        self.canvas.pack(fill=BOTH, expand=1)
 
+    def draw_update(self):
         # here is where we draw stuff
 
-        # For each horizontal line (add 1 because we need 3 lines to draw 2 boxes)
-        for x in range(self.dimensions[0] + 1):
-            canvas.create_line(x * self.size + self.border, self.border, x * self.size + self.border, self.border + self.size * self.dimensions[1])
+        # Draw the vertical cell walls
+        for x in range(len(self.vertical)):
+            for y in range(len(self.vertical[x])):
+                if self.vertical[x][y] == 1:
+                    col = self.color_normal_stroke
+                if self.vertical[x][y]:
+                    self.canvas.create_line(self.border + x * self.size, self.border + y * self.size,
+                                       self.border + x * self.size, self.border + (y + 1) * self.size, fill=col)
 
-            # For each vertical line
-            for y in range(self.dimensions[1] + 1):
-                canvas.create_line(self.border, y * self.size + self.border, self.border + self.size * self.dimensions[0], y * self.size + self.border)
+        # Draw the horizontal cell walls
+        for x in range(len(self.horizontal)):
+            for y in range(len(self.horizontal[x])):
+                if self.horizontal[x][y] == 1:
+                    col = self.color_normal_stroke
+                if self.horizontal[x][y]:
+                    self.canvas.create_line(self.border + x * self.size, self.border + y * self.size,
+                                       self.border + (x + 1) * self.size, self.border + y * self.size, fill=col)
 
+        # Fill the cells
+        for x in range(len(self.cells)):
+            for y in range(len(self.cells[x])):
+                if self.cells[x][y] == 0:
+                    col = self.color_maze_fill
+                elif self.cells[x][y] == 2:
+                    col = self.color_select_fill
+                elif self.cells[x][y] == 1:
+                    col = self.color_normal_fill
+                self.canvas.create_rectangle(self.border + x * self.size, self.border + y * self.size,
+                                        self.border + (x + 1) * self.size, self.border + (y + 1) * self.size, fill=col)
 
+    def select_cell(self, x, y):
+        self.cells[x][y] = 2
+        self.draw_update()
 
-        canvas.pack(fill=BOTH, expand=1)
-
-    def move(self):
-        self.x += 1;
-        self.y += 1;
+    def add_to_maze(self, x, y):
+        self.cells[x][y] = 0
+        self.draw_update()
 
 
 
--- Python/Mazes/Prim_Generator.py
+++ Python/Mazes/Prim_Generator.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from MazeStructure import MazeFrame;
+from MazeStructure import MazeFrame
+import random
 
 # Creating a Tkinter window to hold the GUI
 window = Tk()
@@ -11,5 +12,11 @@ size = 30
 
 # Creating an empty frame, passing in the window, dimensions of maze, and size of cell
 maze = MazeFrame("Prim's Maze Generation", window, dimensions, size)
+
+
+cx, cy = random.randint(0, dimensions[0]-1), random.randint(0, dimensions[1]-1)
+maze.select_cell(cx, cy)
+maze.add_to_maze(cx, cy)
+
 
 window.mainloop()
