5b1314c7aa1a202842390715fc8583dd9ed1f3a1
Luke Hanna <Luke.m.hanna@gmail.com>
2021-06-08 19:12:58 -0400

    Added a toString function to allow you to view the gamestate as text

--- Java/Games/Connect4/src/ConnectFour.java
+++ Java/Games/Connect4/src/ConnectFour.java
@@ -15,6 +15,44 @@ public class ConnectFour
 
     }
 
+    /**
+     *
+     *  For use printing the gamestate.
+     *
+     * @return Gamestate representation as a multi-line string
+     */
+    public String toString() {
 
+        // This string will represent the gamestate
+        String boardString = "";
+
+        // Saving the value for the newline element from OS
+        String newLine = System.getProperty("line.separator");
+
+        // For each row
+        for (int y = 0; y < boardy; y++) {
+
+            // Create an element to store the line
+            String line = "";
+
+            // For each column item of each row
+            for (int x = 0; x < boardx; x++) {
+
+                // Add the correct value and a space to the line
+                line += board[x][y] + " ";
+
+            }
+
+            // add the line to the final string
+            boardString += line;
+
+            // and then add the newline element
+            boardString += newLine;
+
+        }
+
+        // Return the completed multiline string representing gamestate
+        return boardString;
+    }
 
 }
