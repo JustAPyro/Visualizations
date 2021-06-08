77e22d41189812f054be7fd904ee3d204ec51c34
Luke Hanna <Luke.m.hanna@gmail.com>
2021-06-08 19:07:15 -0400

    Set up variables for the connect game state

--- Java/Games/Connect4/src/ConnectFour.java
+++ Java/Games/Connect4/src/ConnectFour.java
@@ -1,9 +1,20 @@
 public class ConnectFour
 {
 
+    // Width and height of the board (Standard is 6x7)
+    int boardx = 7; int boardy = 6;
+
+    // Declaring the variable for the game board
+    int[][] board;
+
     // Constructor Class
     public ConnectFour() {
 
+        // Initializing the board
+        board = new int[boardx][boardy];
+
     }
+
+
 
 }
