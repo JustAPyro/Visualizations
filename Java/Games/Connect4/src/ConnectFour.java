4a0a7c065e8bcb866c94c745e157bf67086521a7
Luke Hanna <Luke.m.hanna@gmail.com>
2021-06-08 19:21:56 -0400

    Create an insert method that allows you to add a piece to the board

--- Java/Games/Connect4/src/ConnectFour.java
+++ Java/Games/Connect4/src/ConnectFour.java
@@ -2,7 +2,7 @@ public class ConnectFour
 {
 
     // Width and height of the board (Standard is 6x7)
-    private int boardx = 7; private int boardy = 6;
+    private final int boardx = 7; private final int boardy = 6;
 
     // Declaring the variable for the game board
     private int[][] board;
@@ -15,7 +15,36 @@ public class ConnectFour
 
     }
 
+    /**
+     * Allows you to add pieces to the game board to try to 'Connect Four'
+     *
+     * @param location The location you want to set the piece (0-start indexed)
+     * @param player The player making the move
+     * @throws IndexOutOfBoundsException if you try to insert a piece at an invalid point
+     */
+    public void insert(int location, int player) {
 
+        // First, check if the given location is out of bounds
+        if (location < 0 || location > boardx)
+
+            // Throw an IndexOutOfBoundsException
+            throw new IndexOutOfBoundsException("Tried to insert piece at invalid point!");
+
+        // Create a variable to track the correct insertion height
+        int insertHeight = boardy - 1;
+
+        // Starting from the bottom of the board, as long as there's a piece at insertHeight
+        while (board[location][insertHeight] != 0) {
+
+            // Increment insertHeight to move one up
+            insertHeight--;
+
+        }
+
+        // Finally, once we have the insert height set that location to belong to the given player
+        board[location][insertHeight] = player;
+
+    }
 
     /**
      *
@@ -27,7 +56,7 @@ public class ConnectFour
     public String toString() {
 
         // This string will represent the gamestate
-        String boardString = "";
+        StringBuilder boardString = new StringBuilder();
 
         // Saving the value for the newline element from OS
         String newLine = System.getProperty("line.separator");
@@ -36,26 +65,26 @@ public class ConnectFour
         for (int y = 0; y < boardy; y++) {
 
             // Create an element to store the line
-            String line = "";
+            StringBuilder line = new StringBuilder();
 
             // For each column item of each row
             for (int x = 0; x < boardx; x++) {
 
                 // Add the correct value and a space to the line
-                line += board[x][y] + " ";
+                line.append(board[x][y]).append(" ");
 
             }
 
             // add the line to the final string
-            boardString += line;
+            boardString.append(line);
 
             // and then add the newline element
-            boardString += newLine;
+            boardString.append(newLine);
 
         }
 
         // Return the completed multiline string representing gamestate
-        return boardString;
+        return boardString.toString();
     }
 
 }
