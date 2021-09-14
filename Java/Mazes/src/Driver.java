6af1e2d6530a397cf459fe1bf78192708b59388d
Luke Hanna <Luke.m.hanna@gmail.com>
2021-09-13 23:49:17 -0400

    Set up driver class to correctly call GUI

--- Java/Mazes/src/Driver.java
+++ Java/Mazes/src/Driver.java
@@ -1,3 +1,5 @@
+import javafx.application.Application; // Need this to call application.launch
+
 // Launches the Maze
 public class Driver
 {
@@ -6,7 +8,7 @@ public class Driver
     public static void main(String[] args)
     {
         // Launch the GUI, passing in the the args and GUI class, as well as the maze generator of choice
-   
+        Application.launch(GUI.class, args);
 
     }
 
