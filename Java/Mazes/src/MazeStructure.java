3d8fa89dc04dfb3f87f14158446f4678ba0aac6a
Luke Hanna <Luke.m.hanna@gmail.com>
2021-09-14 11:06:21 -0400

    Spread wall object to it's own Class file

--- Java/Mazes/src/MazeStructure.java
+++ Java/Mazes/src/MazeStructure.java
@@ -32,23 +32,7 @@ public class MazeStructure
     int[][] vWalls;     // Represents vertical maze walls
     int[][] cells;    // Represents all maze cell status
 
-    /**
-     * This class represents a specific wall to be used by maze structure
-     */
-    private class Wall
-    {
-        char o;
-        int x, y;
 
-        // Each wall can be represented by an orientation and and x, y coordinate
-        public Wall(char o, int x, int y)
-        {
-            // Save provided variables
-            this.o = o;
-            this.x = x;
-            this.y = y;
-        }
-    }
 
     // Create a list of saved walls
     ArrayList<Wall> saved = new ArrayList<Wall>();
