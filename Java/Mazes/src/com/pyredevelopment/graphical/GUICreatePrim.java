b836ec4d48882992abcd79ba7402e5aa9a82cad2
Luke Hanna <Luke.m.hanna@gmail.com>
2021-09-24 14:29:22 -0400

    Correctly commented and labeled Prim's GUI, including handling all warnings.

--- Java/Mazes/src/com/pyredevelopment/graphical/GUICreatePrim.java
+++ Java/Mazes/src/com/pyredevelopment/graphical/GUICreatePrim.java
@@ -1,26 +1,33 @@
 package com.pyredevelopment.graphical;
 
 import com.pyredevelopment.algorithms_generators.GeneratorPrim;
-import com.pyredevelopment.cutility.ResizableCanvas;
 import javafx.stage.Stage;
 
+/**
+ * This class represents the visualization of maze generation using a randomized prim's algorithm.
+ * Implementation by Luke Hanna (Github.com/JustAPyro / PyreDevelopment.com) on Sept 2021
+ */
 public class GUICreatePrim extends GUI
 {
 
     /**
-     * This is entered when the application is launched
+     * This is entered when the application is launched, by editing the parent variables, we can make this window
+     * an appropriate way to display visualizations of most maze generation or solving techniques.
      *
      * @param primaryStage The main window of the application
      */
     @Override
     public void start(Stage primaryStage)
     {
+        // Initialize our maze related GUI
         init();
 
-        titleString = "Prim's Maze Generation";
-        headerString = "Prim's Maze Generation";
-        goalString = "Maze";
+        // Declare GUI info
+        titleString = "Prim's Maze Generation";     // Window title
+        headerString = "Prim's Maze Generation";    // Window Header
+        goalString = "Maze";                        // Complete ... ? Button
 
+        // Insert the Prim's algorithm text to solve this
         // Create a text manager and attach it to textCanvas
         tm = new TextManager(textCanvas);
         tm.addText(1, 0, "1. Start with a grid full of walls");
@@ -34,6 +41,7 @@ public class GUICreatePrim extends GUI
         // Then create a Prim generator and attach it to mazeCanvas
         alg = new GeneratorPrim(mazeCanvas, tm);
 
+        // Run the super GUI constructor
         super.start(primaryStage);
     }
 }
