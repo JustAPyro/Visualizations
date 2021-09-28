90d14e2424aa6f2962b9fcab66d6c898e8227322
Luke Hanna <Luke.m.hanna@gmail.com>
2021-09-27 23:47:49 -0400

    Created a driver for the breadth first serach class

---
+++ Java/Mazes/src/com/pyredevelopment/drivers/BFSDriver.java
@@ -0,0 +1,25 @@
+package com.pyredevelopment.drivers;
+
+import com.pyredevelopment.graphical.GUISolveDFS;
+import javafx.application.Application;
+
+/**
+ * This is a driver for the breadth first search visulization GUI
+ *
+ * @author Luke Hanna (Github.com/JustAPyro | PyreDevelopment.com)
+ * @version 1.0 | Created s{DATE} | Updated s{DATE}
+ */
+public class BFSDriver
+{
+
+    /**
+     * Main entry point
+     * @param args Arguments
+     */
+    public static void main(String[] args)
+    {
+        // call Application.launch to initialized a JFX thread and launch the GUI
+        Application.launch(GUISolveBFS.class, args);
+    }
+
+}
