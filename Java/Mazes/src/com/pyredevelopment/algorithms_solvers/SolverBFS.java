251785f44383214824a03c48128a018338b0f162
Luke Hanna <Luke.m.hanna@gmail.com>
2021-09-27 23:55:23 -0400

    Created a maze algorithm class for the BFS algorithm

---
+++ Java/Mazes/src/com/pyredevelopment/algorithms_solvers/SolverBFS.java
@@ -0,0 +1,38 @@
+package com.pyredevelopment.algorithms_solvers;
+
+import com.pyredevelopment.maze.MazeAlgorithm;
+
+/**
+ * Implementation of a breadth First Search in maze solving, using com.pyredevelopment.maze.MazeStructure
+ * Objective: Create an easily understandable visualizations of breadth first searching to solve a maze.
+ *
+ * Details: TODO: Fill this out.
+ *
+ * You can find the write up of the original plans for this project here:
+ * TODO: Add original plans
+ *
+ * @author Luke Hanna (Github.com/JustAPyro | PyreDevelopment.com)
+ * @version 1.0 | Created s{DATE} | Updated s{DATE}
+ */
+public class SolverBFS extends MazeAlgorithm
+{
+
+
+    @Override
+    public void newMazeButton()
+    {
+
+    }
+
+    @Override
+    public boolean isComplete()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean nextStep()
+    {
+        return false;
+    }
+}
